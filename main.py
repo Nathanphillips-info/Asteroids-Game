24cbef214d6baf8c70696562d47c60142c3f228f
Nathan Phillips <Nathan@Admins-MacBook-Air.local>
2026-01-01 20:19:06 -0700

    screen fps

--- main.py
+++ main.py
@@ -6,14 +6,21 @@ def main():
     print("Starting Asteroids with pygame version: VERSION")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    
+    clock = pygame.time.Clock()
+    dt = 0
+    
     while True:
+        screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+        
+        
 
 
 if __name__ == "__main__":
