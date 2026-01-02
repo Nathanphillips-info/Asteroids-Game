429a3cbf025fced267c46700d3bc675c50ebc052
Nathan Phillips <Nathan@Admins-MacBook-Air.local>
2026-01-02 08:05:29 -0700

    added player to frames

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import pygame 
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
+
 def main():
     pygame.init()
     print("Starting Asteroids with pygame version: VERSION")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-    
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     clock = pygame.time.Clock()
     dt = 0
     
@@ -17,8 +19,12 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
+       
+
+    
         
         
 
